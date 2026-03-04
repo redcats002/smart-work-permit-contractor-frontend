ffc3b2fd15d3b5cfb2380bc3a3e75ba0403dfaae
Kan <kan@makewebbkk.com>
2026-03-04 18:48:42 +0700

    ✨ feat(ui): add BaseContainer, BaseTop, and BackButton components with responsive design and slot support

---
+++ src/components/base/BaseContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="
+      w-full
+      bg-white
+      border border-[#E2E8F0] rounded-md
+      px-4 py-6
+      sm:p-3
+      md:p-5
+      lg:p-10
+    ">
+    <slot />
+  </div>
+</template>
---
+++ src/components/base/BaseTop.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-2.5">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
