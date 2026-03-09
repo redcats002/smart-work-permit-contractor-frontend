573da75cebc5e2db60ebb24c2a68ec595dae26c5
Kan <kan@makewebbkk.com>
2026-03-09 15:56:56 +0700

    ✨ feat(income-category): implement IncomeCategory and IncomeType components with CRUD functionality

---
+++ src/pages/setting/pages/financial/pages/income-category/components/type/IncomeTypeMenuAction.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <BaseActionMenu :items="items" />
+    <ModalIncomeTypeAction
+      v-if="item.id"
+      :id="item.id"
+      v-model="form"
+      v-model:visible="editModalVisible"
+      :type="'EDIT'"
+      @edit="onEdit($event)" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { IActionFinanceIncomeTypePayload } from '@/models/request/finance-income-category/FinanceIncomeCategoryReq.model'
+import type { IFinanceIncomeTypeList } from '@/models/response/finance-income-type/FinanceIncomeTypeRes.model'
+import BaseActionMenu, { type IMenuItemAction } from '@/components/base/BaseActionMenu.vue'
+import ModalIncomeTypeAction from './ModalIncomeTypeAction.vue'
+
+interface IProps {
+  item: IFinanceIncomeTypeList
+}
+
+interface IEmits {
+  edit: [id: number]
+  delete: [id: number]
+}
+
+const props = defineProps<IProps>()
+const emits = defineEmits<IEmits>()
+
+const form = defineModel<IActionFinanceIncomeTypePayload>({ required: true })
+const editModalVisible = ref<boolean>(false)
+
+const items = ref<IMenuItemAction[]>([
+  { label: 'แก้ไข', key: 'edit', type: 'TEXT', action: (): void => { openEdit() } },
+  { label: 'ลบ', key: 'delete', action: (): void => { emits('delete', props.item.id!) }, type: 'DELETE' }
+])
+
+function openEdit (): void {
+  form.value = JSON.parse(JSON.stringify(props.item))
+  editModalVisible.value = true
+}
+
+function onEdit (id: number): void {
+  emits('edit', id)
+}
+
+</script>
+
+<style scoped></style>
