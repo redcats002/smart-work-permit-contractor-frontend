2a2b0c8c55e20b871d84e95fd663a81b3db90024
Kan <kan@makewebbkk.com>
2026-03-10 18:34:17 +0700

    ✨ feat(filters): update model definition to include 'search' for filter components

--- src/pages/setting/pages/other/pages/warehouse/pages/list/components/WarehouseFilter.vue
+++ src/pages/setting/pages/other/pages/warehouse/pages/list/components/WarehouseFilter.vue
@@ -50,7 +50,7 @@ interface IEmits {
 
 const emits = defineEmits<IEmits>()
 
-const model = defineModel<string>({ default: '' })
+const model = defineModel<string>('search', { default: '' })
 defineModel<IWarehouseFilter>('filter', { default: (): IWarehouseFilter => ({}) })
 
 function onSearch (): void {
