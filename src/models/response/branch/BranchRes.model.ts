7d9bc5975ca4c9661e3de561fa979f99005d7ac4
Kan <kan@makewebbkk.com>
2026-03-11 15:52:54 +0700

    ✨ feat(branch): add branch management models, provider, and router

---
+++ src/models/response/branch/BranchRes.model.ts
@@ -0,0 +1,25 @@
+import type { IEntity } from '@/models/Global.model'
+import type { IBranchTime } from '@/models/modules/branch/BranchTime.model'
+import type { TBranchStatus } from '@/enums/modules/branch/BranchStatus.enum'
+import type { IBasePaginationResponse, IBaseSuccessResponse } from '../Response.model'
+
+export interface IBranchList extends IEntity {
+  name: string
+  status: TBranchStatus
+}
+export interface IBranchById extends IEntity {
+  status: TBranchStatus
+  name: string
+  taxId: string
+  openAt: string
+  address: string
+  district: string
+  subDistrict: string
+  province: string
+  postCode: string
+  branchTimes: IBranchTime[]
+}
+
+export type TGetBranchListResponse = IBasePaginationResponse<IBranchList>
+export type TGetBranchByIdResponse = IBaseSuccessResponse<IBranchById>
+export type TActionBranch = IBaseSuccessResponse<boolean>
