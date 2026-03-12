4c3f7cf2bf064b1792a7d2d75fc4de255773cdd8
BuraphaS <burapha.s2545@gmail.com>
2026-03-12 17:38:54 +0700

    Feat: ui receipt and expenses list

---
+++ src/pages/finance/pages/record/pages/RecordExpensesListPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="invoice-list-page">
+    <PageTitle />
+    <BasePage>
+      <div class="mb-4">
+        <SearchInput
+          v-model="search"
+          @search="fetch()" />
+      </div>
+      <RecordExpenses
+        v-model:pagination="pagination"
+        v-model:sort-by="sortBy"
+        v-model:sort-order="sortOrder"
+        :items="items"
+        @update="fetch()" />
+    </BasePage>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import BasePage from '@/components/base/BasePage.vue'
+import SearchInput from '@/components/input/SearchInput.vue'
+import PageTitle from '@/components/nav/PageTitle.vue'
+import RecordExpenses from '../components/RecordExpensesTable.vue'
+import useList from '../composables/useList'
+
+const { items, pagination, sortBy, sortOrder, search, fetch } = useList()
+
+onMounted((): void => {
+  fetch()
+})
+</script>
