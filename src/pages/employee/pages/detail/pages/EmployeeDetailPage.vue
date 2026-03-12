4fdf1c975230352ab877430084cb18544bfc211f
chet_softnova <chet@softnova.co>
2026-03-12 14:21:00 +0700

    fix: mock up data

--- src/pages/employee/pages/detail/pages/EmployeeDetailPage.vue
+++ src/pages/employee/pages/detail/pages/EmployeeDetailPage.vue
@@ -17,7 +17,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { toast } from '@/plugins/toast'
 import { handleLoading } from '@/utils/HandleLoading'
@@ -30,6 +30,7 @@ import Spacer from '@/components/flex/Spacer.vue'
 import PageTitle from '@/components/nav/PageTitle.vue'
 import InformationDetail from '../components/InformationDetail.vue'
 import { useInitDetail } from '../composables/useInitDetail'
+import type { IEmployeeById } from '@/models/response/employee/EmployeeRes.model'
 
 const route = useRoute()
 const router = useRouter()
@@ -39,9 +40,49 @@ const EmployeeService: IEmployeeProvider = new EmployeeProvider()
 const employee = useInitDetail()
 const employeeId = computed((): number => Number(route?.params?.id as string ?? ''))
 
+
+const mockEmployeeDetail = ref<IEmployeeById>({
+  id: 1,
+  createdAt: '2026-03-12T07:00:00.000Z',
+  updatedAt: '2026-03-12T07:00:00.000Z',
+  status: 'ACTIVE',
+  idCard: '1700401323201',
+  titleName: 'MR',
+  firstName: 'เจษฎากร',
+  lastName: 'เมืองนาม',
+  phoneNumber: '0823636036',
+  birthDate: '2000-08-17',
+  email: 'chet@softnova.co',
+  role: 'SUPER_ADMIN',
+  branchId: 101,
+  mainAddress: {
+    id: 1,
+    address: '7/1 หมู่ 5',
+    subDistrict: 'หนองสองห้อง',
+    district: 'บ้านแพ้ว',
+    province: 'สมุทรสาคร',
+    postCode: '74120',
+    urlGoogleMap: '',
+    isSameCitizenAddress: true,
+    isSameCurrentAddress: true
+  },
+  currentAddress: {
+    id: 2,
+    address: '7/1 หมู่ 5',
+    subDistrict: 'หนองสองห้อง',
+    district: 'บ้านแพ้ว',
+    province: 'สมุทรสาคร',
+    postCode: '74120',
+    urlGoogleMap: '',
+    isSameCitizenAddress: true,
+    isSameCurrentAddress: true
+  }
+})
+
 async function useFetch (): Promise<void> {
-  const { data } = await EmployeeService.getEmployeeFindOne(employeeId.value)
-  employee.value = useInitDetail(data).value
+  // const { data } = await EmployeeService.getEmployeeFindOne(employeeId.value)
+  // employee.value = useInitDetail(data).value
+  employee.value = useInitDetail(mockEmployeeDetail.value).value
 }
 
 async function useDelete (): Promise<void> {
