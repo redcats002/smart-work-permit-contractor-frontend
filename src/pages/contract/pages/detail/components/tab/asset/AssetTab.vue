262ef89064e415d478392968c03cc0f31499216e
Kan <kan@makewebbkk.com>
2026-03-14 23:05:44 +0700

    ✨ feat: add Asset management components including AssetFilter, AssetTab, AssetTable, and ChipAssetStatus for enhanced contract asset handling

---
+++ src/pages/contract/pages/detail/components/tab/asset/AssetTab.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="grid gap-2.5">
+    <EstateFilter
+      v-model:filters="filters"
+      v-model:search="search"
+      @clear="onClearFilters()"
+      @search="fetch()" />
+    <EstateTable
+      v-model:pagination="pagination"
+      v-model:sort-by="sortBy"
+      v-model:sort-order="sortOrder"
+      :items="items"
+      @update="fetch()" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { handleLoading } from '@/utils/HandleLoading'
+import type { IGetCustomerEstateList, IGetCustomerList } from '@/models/request/customer/CustomerReq.model'
+import type { ICustomerEstateList } from '@/models/response/customer/CustomerRes.model'
+import CustomerProvider, { type ICustomerProvider } from '@/resources/provider/customer/Customer.provider'
+import usePagination from '@/composables/usePagination'
+import EstateFilter from './AssetFilter.vue'
+import EstateTable from './AssetTable.vue'
+
+const CustomerService: ICustomerProvider = new CustomerProvider()
+
+const route = useRoute()
+const { search, pagination, sortBy, sortOrder, extractPagination, syncQuery } = usePagination()
+
+const filters = ref<IGetCustomerEstateList>({})
+const items = ref<ICustomerEstateList[]>([])
+const customerId = computed((): number => route?.params?.id ? Number(route.params.id) : 0)
+
+const paginateQuery = computed((): IGetCustomerEstateList => {
+  const normalizedFilters = normalizeFilters(filters.value)
+  return {
+    search: search.value,
+    page: pagination.value.page,
+    limit: pagination.value.limit,
+    sortBy: sortBy.value || undefined,
+    sortOrder: sortOrder.value,
+    ...normalizedFilters
+  }
+})
+
+async function useFetch (): Promise<void> {
+  const mock = true // TODO: remove mock when api ready
+  if (mock) {
+    items.value = []
+    return
+  }
+  const response = await CustomerService.getCustomerEstates(customerId.value, paginateQuery.value)
+  items.value = response?.data || []
+  pagination.value = extractPagination(response)
+  syncQuery({ ...normalizeFilters(filters.value) })
+}
+
+
+function normalizeFilters (value: IGetCustomerList): Partial<IGetCustomerList> {
+  return {
+    ...value
+  }
+}
+
+function fetch (): void {
+  handleLoading(useFetch)
+}
+
+function onClearFilters (): void {}
+
+
+</script>
+
+<style scoped>
+
+</style>
