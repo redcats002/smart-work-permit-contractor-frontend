a9a27acd27238bb1ab8913aad3c45b5a9778d371
chet_softnova <chet@softnova.co>
2026-03-16 11:36:15 +0700

    fix: responsive

--- src/pages/stock/pages/detail/components/InformationDetail.vue
+++ src/pages/stock/pages/detail/components/InformationDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-2 gap-2.5">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-2.5">
     <BaseContainer>
       <DisplayList :items="leftItems">
         <template #[`value.status`]>
