cdf9f2c84a3acb8fcad6c2941b67014cf11f998f
hart_softnova <hart@softnova.co>
2026-03-18 15:45:45 +0700

    fix: font

--- src/pages/reports/pages/list/pages/ReportListPage.vue
+++ src/pages/reports/pages/list/pages/ReportListPage.vue
@@ -1,7 +1,7 @@
 <template>
   <section
     id="report-list-page"
-    class="bg-[#FAFAFE] font-['Sukhumvit_Set']">
+    class="bg-[#FAFAFE]">
     <PageTitle />
     <BasePage>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-6">
