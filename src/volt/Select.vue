53cfd52574ef5bead0e7c475225078d6cb95a8d7
Kan <kan@makewebbkk.com>
2026-03-18 14:25:56 +0700

    fix: add disabled styles to Select component for better accessibility

--- src/volt/Select.vue
+++ src/volt/Select.vue
@@ -97,6 +97,7 @@ const theme = ref<SelectPassThroughOptions>({
         px-3 py-2 border-none text-surface-700 dark:text-surface-0 bg-transparent rounded-sm
         p-focus:bg-surface-100 dark:p-focus:bg-surface-800 p-focus:text-surface-800 dark:p-focus:text-surface-0
         p-selected:bg-highlight p-focus:p-selected:bg-highlight-emphasis
+        p-disabled:opacity-50 p-disabled:pointer-events-none p-disabled:cursor-not-allowed
         transition-colors duration-200`,
 	optionLabel: ``,
 	optionCheckIcon: `relative -ms-[0.375rem] me-[0.375rem] text-surface-700 dark:text-surface-0`,
