357a8365b54b5c4923b3cc5d96afa89fb30254f2
Kan <kan@makewebbkk.com>
2026-03-20 12:59:22 +0700

    ✨ feat(AnnualFinanceReceipt): Implement models, provider, and router for annual finance receipt management

---
+++ src/resources/provider/report/AnnualFinanceReceipt.provider.ts
@@ -0,0 +1,18 @@
+import type { IGetAnnualFinanceReceiptList } from '@/models/request/report/annual-finance-receipt/AnnualFinanceReceiptReq.model'
+import type { TGetAnnualFinanceReceiptListResponse } from '@/models/response/report/annual-finance-receipt/AnnualFinanceReceiptRes.model'
+import HttpRequest from '@/resources/HttpRequest'
+
+export interface IAnnualFinanceReceiptProvider {
+  getAnnualFinanceReceiptPaginate(query: IGetAnnualFinanceReceiptList): Promise<TGetAnnualFinanceReceiptListResponse>
+}
+
+class AnnualFinanceReceiptProvider extends HttpRequest implements IAnnualFinanceReceiptProvider {
+  private urlPrefix: string = '/api/v1/report/annual-finance-receipt'
+
+  public async getAnnualFinanceReceiptPaginate (query: IGetAnnualFinanceReceiptList): Promise<TGetAnnualFinanceReceiptListResponse> {
+    const response = await this.get(`${this.urlPrefix}`, query)
+    return response
+  }
+}
+
+export default AnnualFinanceReceiptProvider
