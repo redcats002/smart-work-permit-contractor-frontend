0739d1ebfcfc926cd3ff15b8670e8af621217845
Kan <kan@makewebbkk.com>
2026-03-24 19:08:23 +0700

    🩹 fix: update Divider component styles for improved theming consistency

--- src/volt/Divider.vue
+++ src/volt/Divider.vue
@@ -17,11 +17,11 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { ptViewMerge } from './utils'
 import Divider, {
   type DividerPassThroughOptions,
   type DividerProps
 } from 'primevue/divider'
+import { ptViewMerge } from './utils'
 
 interface Props extends /* @vue-ignore */ DividerProps {}
 defineProps<Props>()
@@ -30,7 +30,7 @@ const theme = ref<DividerPassThroughOptions>({
   root: `p-horizontal:flex p-horizontal:w-full p-horizontal:relative p-horizontal:items-center 
         p-horizontal:my-4 p-horizontal:mx-0p-horizontal:py-0 p-horizontal:px-4
         p-horizontal:before:absolute p-horizontal:before:block p-horizontal:before:top-1/2 p-horizontal:before:start-0 p-horizontal:before:w-full
-        p-horizontal:before:border-t p-horizontal:before:border-surface-200 dark:p-horizontal:before:border-surface-700
+        p-horizontal:before:border-t p-horizontal:before:border-primary-500 dark:p-horizontal:before:border-primary-700
         p-vertical:min-h-full p-vertical:flex p-vertical:relative p-vertical:justify-center
         p-vertical:my-0 p-vertical:mx-4 p-vertical:py-2 p-vertical:px-0
         p-vertical:before:absolute p-vertical:before:block p-vertical:before:top-0 p-vertical:before:start-1/2 p-vertical:before:h-full
