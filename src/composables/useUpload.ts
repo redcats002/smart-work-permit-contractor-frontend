6648025f7a57d0dd2f46d885dcec87ca752df4d0
Kan <kan@makewebbkk.com>
2026-03-26 18:10:13 +0700

    🐛 fix: update upload handling in useUpload and UploadProvider for improved response structure

--- src/composables/useUpload.ts
+++ src/composables/useUpload.ts
@@ -1,43 +1,35 @@
-import { type Ref } from 'vue'
-import UploadProvider, { type IMedia } from '@/resources/provider/Upload.provider'
+import { ref, type Ref } from 'vue'
+import UploadProvider, {
+  type IUploadProvider,
+  type IUploadResponse
+} from '@/resources/provider/Upload.provider'
 
 export interface IUseUpload {
-  uploadAndReplaceImages (images: Ref<IMedia[]>): Promise<Ref<IMedia[]>>
-  getUploadImages (images: IMedia[]): Promise<IMedia[]>
+  files: Ref<File[]>
+  previewUrls: Ref<string[]>
+  getUploadImages (): Promise<IUploadResponse[]>
 }
 
 export default function useUpload (): IUseUpload {
-  const UploadService = new UploadProvider()
+  const UploadService: IUploadProvider = new UploadProvider()
 
-  async function uploadAndReplaceImages (images: Ref<IMedia[]>): Promise<Ref<IMedia[]>> {
-    for (const i in images.value) {
-      const image = images.value?.[i]
-      if (!image) continue
-      if (image?.file) {
-        const response = await UploadService.uploadFile(image.file)
-        if (!response?.data?.url) continue
-        images.value?.splice(Number(i), 1, { ...image, ...response.data })
+  const files = ref<File[]>([])
+  const previewUrls = ref<string[]>([])
+
+  async function getUploadImages (): Promise<IUploadResponse[]> {
+    const uploadResults: IUploadResponse[] = []
+    for (const file of files.value) {
+      const response = await UploadService.uploadFile(file)
+      if (response && response.data) {
+        uploadResults.push(response.data)
       }
     }
-    return images
-  }
-
-
-  async function getUploadImages (images: IMedia[]): Promise<IMedia[]> {
-    for (const i in images) {
-      const image = images?.[i]
-      if (!image) continue
-      if (image?.file) {
-        const response = await UploadService.uploadFile(image.file as any)
-        if (!response.data?.name) continue
-        images?.splice(Number(i), 1, { ...image, ...response.data })
-      }
-    }
-    return images
+    return uploadResults
   }
 
   return {
-    uploadAndReplaceImages,
+    files,
+    previewUrls,
     getUploadImages
   }
 }
