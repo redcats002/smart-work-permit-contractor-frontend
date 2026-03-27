e8fb289ee2c40e53c1c8d5a1aab535da55b26fc5
Kan <kan@makewebbkk.com>
2026-03-27 14:47:56 +0700

    ✨ feat: implement MultiSelect component with customizable icons and themes

--- src/components/input/AutoCompleteInput.vue
+++ src/components/input/AutoCompleteInput.vue
@@ -31,23 +31,7 @@
           @click="removeCallback($event)" />
       </span>
     </template>
-    <!-- <template
-      v-if="dropdown"
-      #dropdown="{ props: dropdownProps }">
-      <div
-        :class="dropdownClass"
-        :data-p="$attrs?.invalid ? 'invalid' : ''"
-        v-bind="dropdownProps">
-        <Icon
-          class="size-5"
-          icon="mdi:chevron-down" />
-      </div>
-    </template> -->
-    <!-- <template #dropdownicon>
-      <Icon
-        class="size-5 text-[#A4B0C1]"
-        icon="mdi:chevron-down" />
-    </template> -->
+    <!-- !DEPRECATED better use MultiSelectInput -->
     <template
       v-if="$attrs?.multiple"
       #option="{ option }">
@@ -67,7 +51,6 @@
 </template>
 
 <script setup lang="ts">
-// import { computed } from 'vue'
 import type { TBaseModel } from '@/models/Global.model'
 import AutoComplete from '@/volt/AutoComplete.vue'
 import { Icon } from '@iconify/vue'
@@ -88,17 +71,6 @@ withDefaults(defineProps<IProps>(), {
 })
 
 const model = defineModel<TBaseModel | TBaseModel[] | null>()
-
-// const dropdownClass = computed((): string => {
-//   return `cursor-pointer inline-flex items-center justify-center select-none overflow-hidden relative w-10 shrink-0 rounded-e-md
-//         border border-s-0 border-surface-300 dark:border-surface-700
-//         group-focus-within:border-primary
-// p-invalid:border-red-400 dark:p-invalid:border-red-300
-//         bg-white dark:bg-surface-800
-//         text-surface-600 dark:text-surface-300
-//         transition-colors duration-200`
-//   // group-hover:border-surface-400 dark:group-hover:border-surface-600  //TODO: (put it on root) but it not group with dropdown
-// })
 
 function isSelected (option: TBaseModel): boolean {
   if (Array.isArray(model.value)) return model.value.some((item: TBaseModel): boolean => item?.id === option.id)
