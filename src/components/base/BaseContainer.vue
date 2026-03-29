64d8b9f94bec55903ac218c32677eccfe5b528df
Kan <kan@makewebbkk.com>
2026-03-29 20:51:45 +0700

    ♻️ refactor: simplify class binding in BaseContainer component for improved readability

--- src/components/base/BaseContainer.vue
+++ src/components/base/BaseContainer.vue
@@ -1,15 +1,9 @@
 <template>
   <div
-    class="
-      relative
-      w-full
-      bg-white
-      border border-[#E2E8F0] rounded-md
-      px-4 py-6
-      sm:p-3
-      md:p-5
-      lg:p-10
-    ">
+    :class="{
+      'bg-shadow': shadow
+    }"
+    class="relative w-full bg-white rounded-md px-4 py-6 sm:p-3 md:p-5 lg:p-10">
     <div
       v-if="$slots.topright"
       class="absolute mb-4 grid gap-2 right-4 top-4">
@@ -18,3 +12,21 @@
     <slot />
   </div>
 </template>
+<script lang="ts" setup>
+interface IProps {
+  shadow?: boolean
+}
+
+withDefaults(defineProps<IProps>(), {
+  shadow: true
+})
+</script>
+
+<style  scoped>
+.bg-shadow {
+	border-radius: 8px;
+	background: var(--wh, #fff);
+	/* sh */
+	box-shadow: 0 0 10px 4px rgba(16, 68, 212, 0.051);
+}
+</style>
