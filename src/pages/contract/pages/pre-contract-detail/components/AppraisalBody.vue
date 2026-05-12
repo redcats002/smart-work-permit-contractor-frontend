5f87c977c198a52b24074a5195d87e645d6196c4
Kan <kan@makewebbkk.com>
2026-05-12 17:56:20 +0700

    🐛 fix(DisplayList, AppraisalBody): update row class and add value class for improved layout and styling

--- src/pages/contract/pages/pre-contract-detail/components/AppraisalBody.vue
+++ src/pages/contract/pages/pre-contract-detail/components/AppraisalBody.vue
@@ -2,7 +2,8 @@
   <div class="grid grid-cols-1 gap-2.5">
     <DisplayList
       :items="items"
-      row-class="md:grid-cols-4" />
+      row-class="md:grid-cols-8"
+      value-class="md:col-span-7" />
     <AppraisalTable
       v-model:pagination="pagination"
       :count="count"
@@ -30,7 +31,7 @@ const { pagination } = useLocalPagination(props.appraisal.evaluators)
 
 const items = computed((): IDisplayList[] => {
   return [
-    { key: 'detail', label: 'รายละเอียดเพิ่มเติม', value: props.appraisal?.detail || '-' }
+    { key: 'detail', label: 'รายละเอียดเพิ่มเติม', value: `โลเล็ม อิปซัม (lorem ipsum) — เป็นข้อความแทนที่ (placeholder text) ใช้เพื่อลดความสนใจต่อข้อความที่นำมาแสดง สำหรับการแสดงลักษณะของ ฟอนต์ การพิมพ์และการจัดหน้า ข้อความโลเร็ม อิปซัมเป็นข้อความส่วนหนึ่งในภาษาละตินที่แต่งโดย ซิเซโร โดยมีการตัดต่อคำให้ดูเหมือนเป็นข้อความที่ไม่มีความหมายและไม่ใช่ภาษาละตินที่ถูกต้องในออกแบบต่างๆ ถ้าข้อความสามารถอ่านได้ ผู้ดูจะสนใจข้อความจนไม่สนใจ การจัดหน้าและรูปแบบ ดังนั้นผู้ออกแบบจึงควรใช้ข้อความโลเร็ม อิปซัมเพื่อให้ผู้ดูสนใจที่การออกแบบไม่ใช่ข้อความ` }
   ]
 })
 
