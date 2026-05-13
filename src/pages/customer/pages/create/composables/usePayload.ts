196c8e0fc7ec685275ecbe85f8a137af88f7c449
Kan <kan@makewebbkk.com>
2026-05-13 15:55:17 +0700

    🐛 fix: ensure email field is consistently handled in usePayload for customer creation and editing

--- src/pages/customer/pages/create/composables/usePayload.ts
+++ src/pages/customer/pages/create/composables/usePayload.ts
@@ -4,6 +4,7 @@ import type { CustomerFormValues } from '../schema/customer.schema'
 export function usePayload (form: CustomerFormValues): ICreateCustomerPayload {
   return {
     ...form,
+    email: form?.email || undefined,
     customerGroupId: form?.customerGroupId,
     occupationId: form?.occupationId
   }
