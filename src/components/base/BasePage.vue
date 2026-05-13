47492fcb4e76d1adfe3bf98e301702a1499cde7f
Kan <kan@makewebbkk.com>
2026-05-13 14:23:48 +0700

    🐛 fix: update modal width for consistency across filter components

--- src/components/base/BasePage.vue
+++ src/components/base/BasePage.vue
@@ -1,12 +1,21 @@
 <template>
   <div
-    class="mx-auto max-w-6xl pt-4">
+    :class="{
+      'mx-auto max-w-6xl': responsive
+    }"
+    class="pt-4">
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
+interface IProps {
+  responsive?: boolean
+}
 
+withDefaults(defineProps<IProps>(), {
+  responsive: false
+})
 </script>
 
 <style scoped>
