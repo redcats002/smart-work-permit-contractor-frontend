5488b1bb9877bec2d791e02d36a29c5b97f9f28c
Kan <kan@makewebbkk.com>
2026-05-21 18:17:27 +0700

    🩹 fix(DisplayList.vue, useInitDetail.ts): update label classes and standardize tab labels for clarity

--- src/pages/contract/pages/detail/composables/useInitDetail.ts
+++ src/pages/contract/pages/detail/composables/useInitDetail.ts
@@ -82,13 +82,13 @@ export function useInitTabDetail (): IUseTabItems {
 
   const input = computed((): ITabItemComponent[] => [
     { key: 'Information', label: 'รายละเอียด', instance: Information, value: 'information' },
-    { key: 'Asset', label: 'ทรัพย์สิน', instance: Asset, value: 'asset' },
-    { key: 'Installment', label: 'งวดชำระ', instance: Installment, value: 'installment' },
+    { key: 'Asset', label: 'หลักทรัพย์', instance: Asset, value: 'asset' },
+    { key: 'Installment', label: 'ตารางการชำระ', instance: Installment, value: 'installment' },
     { key: 'Expense', label: 'ค่าใช้จ่าย', instance: Expense, value: 'expense' },
     { key: 'Income', label: 'รายได้', instance: Income, value: 'income' },
     { key: 'Guarantor', label: 'ผู้ค้ำประกัน', instance: Guarantor, value: 'guarantor' },
     { key: 'ContactHistory', label: 'ประวัติการติดต่อ', instance: ContactHistory, value: 'contact-history' },
-    { key: 'Document', label: 'เอกสาร', instance: Document, value: 'document' }
+    { key: 'Document', label: 'รายการเอกสาร', instance: Document, value: 'document' }
   ])
 
   return useTabItems(input)
