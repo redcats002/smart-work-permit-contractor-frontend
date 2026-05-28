9cd0b1ec1dda5109403688fa7d42ed83d94d5b9d
Kan <kan@makewebbkk.com>
2026-05-28 12:50:38 +0700

    🩹 fix(tooltip): add tooltip directive and fade-in animation to PrimeVue configuration

--- src/plugins/primevue.plugin.ts
+++ src/plugins/primevue.plugin.ts
@@ -1,4 +1,8 @@
+import type { App } from 'vue'
+import { ToastService } from 'primevue'
 import PrimeVue, { type PrimeVueConfiguration } from 'primevue/config'
+import Tooltip from 'primevue/tooltip'
+import { registerToastService } from './toast'
 
 export const primeVueConfig: PrimeVueConfiguration = {
   unstyled: true,
@@ -29,7 +33,37 @@ export const primeVueConfig: PrimeVueConfiguration = {
     weekHeader: 'สัปดาห์',
     firstDayOfWeek: 1,
     showMonthAfterYear: false
+  },
+  pt: {
+    directives: {
+      tooltip: {
+        root: {
+          class: `hidden w-fit pointer-events-none absolute max-w-48 group drop-shadow-lg animate-fade-in
+                  data-[p-position=top]:py-1 data-[p-position=bottom]:py-1
+                  data-[p-position=left]:px-1 data-[p-position=right]:px-1
+                  `
+        },
+        arrow: {
+          class: `absolute w-0 h-0 border-transparent border-solid border-[.25rem]
+                  group-data-[p-position=top]:border-y-surface-700 group-data-[p-position=top]:-ml-1 group-data-[p-position=top]:border-b-0
+                  group-data-[p-position=bottom]:border-y-surface-700 group-data-[p-position=bottom]:-ml-1 group-data-[p-position=bottom]:border-t-0
+                  group-data-[p-position=left]:border-l-surface-700 group-data-[p-position=left]:-mt-1 group-data-[p-position=left]:border-r-0
+                  group-data-[p-position=right]:border-r-surface-700 group-data-[p-position=right]:-mt-1 group-data-[p-position=right]:border-l-0
+                  `
+        },
+        text: {
+          class: 'break-words whitespace-pre-line bg-surface-700 text-surface-0 px-3 py-2 rounded'
+        }
+      }
+    }
   }
+}
+
+export function registerPrimeVue (app: App<Element>): void {
+  app.directive('tooltip', Tooltip)
+  app.use(PrimeVue, primeVueConfig)
+  app.use(ToastService)
+  registerToastService(app)
 }
 
 export default PrimeVue
