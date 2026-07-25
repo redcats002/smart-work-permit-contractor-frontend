13c9c5267128f1a21c2775d5a3d051a7bfa71046
Kan <kan@makewebbkk.com>
2026-07-25 10:25:40 +0700

    ✨ feat(warehouse.schema): enhance Warehouse form with dynamic options and validation

--- src/pages/setting/pages/other/pages/warehouse/pages/create/components/LocationForm.vue
+++ src/pages/setting/pages/other/pages/warehouse/pages/create/components/LocationForm.vue
@@ -6,6 +6,7 @@
       class="flex items-end flex-wrap gap-5 mb-4 ">
       <Switch
         v-model="model.options[i].isRequirePrefix"
+        :name="`options.${i}.isRequirePrefix`"
         class="mb-2 w-full md:w-fit"
         false-label="ไม่กำหนดตัวย่อ"
         true-label="กำหนดตัวย่อ" />
